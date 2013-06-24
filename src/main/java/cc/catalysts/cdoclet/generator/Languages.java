40533e472508c67caeceeb3574f78a8277dc0b54
Lucas Binter <lucas.binter@catalysts.cc>
2013-06-24 11:03:01 +0200

    renamed as to actionscript to ensure backward compatibility with asdoclet (if you have @as.class javadoc please use old version)

--- src/main/java/cc/catalysts/cdoclet/generator/Languages.java
+++ src/main/java/cc/catalysts/cdoclet/generator/Languages.java
@@ -3,6 +3,6 @@ package cc.catalysts.cdoclet.generator;
 public class Languages {
 	public static final String CSHARP = "cs";
 	public static final String JAVA = "java";
-	public static final String ACTIONSCRIPT = "as";
+	public static final String ACTIONSCRIPT = "actionscript";
 
 }
